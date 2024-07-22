0859e36302ad688277823fdc33222063ecf7e4f7
Upamanyu Mukharji <upamanyu.mukharji@hotmail.com>
2024-07-23 01:30:45 +0530

    initial files

---
+++ src/test/java/com/controltower/mqtt/MqttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controltower.mqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
